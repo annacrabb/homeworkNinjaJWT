1966c28c5ac184dbeb9a6634394252a1dac521de
Anna Crabb <annacrabb92@gmail.com>
2024-06-09 23:10:22 -0400

    updates

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const authRoutes = require('./routes/authRoutes')
+const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
 const app = express();
 
 // middleware
@@ -19,4 +20,6 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 // routes
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.use(cookieParser());
 app.use(authRoutes);
+
