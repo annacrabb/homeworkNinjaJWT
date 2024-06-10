5ae7c8f622f875a0a41ff8b840f21057f1ba9e50
Anna Crabb <annacrabb92@gmail.com>
2024-06-10 15:01:39 -0400

    jwt vids 13 + 14

--- models/User.js
+++ models/User.js
@@ -23,12 +23,25 @@ console.log('new user was created & saved', doc);
     next();
 })
 
-// fire a functions before doc saved to db
+// fire a function before doc saved to db
 userSchema.pre('save', async function (next){
     const salt = await bcrypt.genSalt();
     this.password = await bcrypt.hash(this.password, salt)
     next();
 });
+
+//static method to login user
+userSchema.statics.login = async function(email, password) {
+    const user = await this.findOne({ email });
+    if (user){
+        const auth = await bcrypt.compare(password, user.password);
+        if (auth) {
+            return user
+        }
+        throw Error ('incorrect password');
+    }
+    throw Error('incorrect email');
+}
 
 const User = mongoose.model('user', userSchema);
 
