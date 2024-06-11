24a54cba685405f54bc8979bffc177f8af1365d5
Anna Crabb <annacrabb92@gmail.com>
2024-06-11 09:02:11 -0400

    lesson 15

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+
+const requireAuth = (req, res, next) => {
+    const token = req.cookies.jwt
+
+    //check json webtoken exists and is verified
+
+    if (token) {
+        jwt.verify(token, 'secret communications', (err, decodedToken) => {
+            if (err) {
+                console.log(err.message);
+                res.redirect('/login');
+
+            } else {
+                console.log(decodedToken);
+                next();
+            }
+        })
+    }
+    else {
+        res.redirect('/login');
+    }
+}
+
+module.exports = { requireAuth };
